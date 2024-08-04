15cac9aa67c5cc64068af1e0ceef36332049ef4f
SL-GW <162018972+SL-GW@users.noreply.github.com>
2024-08-04 13:12:56 +0200

    Created first card in Card.js

--- src/Card.js
+++ src/Card.js
@@ -1,5 +1,27 @@
 import React from "react";
+import "./Card.css";
+import swimmer from "./swimmer.png";
+import Star from "./Star.png";
 
 export default function Card() {
-  return <div className="Card">Hello</div>;
+  return (
+    <div className="Card">
+      <div>
+        <img className="swimImage" src={swimmer} alt="female swimmer" />
+      </div>
+      <div className="cardInfo">
+        <img className="star" src={Star} alt="star" />
+        <span>
+          {" "}
+          <span className="rating">5.0</span>
+          <span className="number"> (6)</span>{" "}
+          <span className="country">USA</span>
+        </span>
+        <p className="description">Life lessons with Katie Zaferes</p>
+        <p className="price">
+          <strong>From $136</strong> / person
+        </p>
+      </div>
+    </div>
+  );
 }
