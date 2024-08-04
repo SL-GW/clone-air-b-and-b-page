f6b3d6141caa9a2f0a2a930dd3ad112c7243f81a
SL-GW <162018972+SL-GW@users.noreply.github.com>
2024-08-04 12:45:49 +0200

    Created Card component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import NavBar from "./NavBar";
 import Hero from "./Hero";
+import Card from "./Card";
 
 function App() {
   return (
     <div className="App">
       <NavBar />
       <Hero />
+      <Card />
     </div>
   );
 }
